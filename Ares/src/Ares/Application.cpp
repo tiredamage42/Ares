9ec423da808ea8232ab2f9e76245a1ad5c6d1df5
Andres <andresgomezjr89@gmail.com>
2020-07-07 01:32:37 -0400

    added window events, including key, mouse, resize and close

--- Ares/src/Ares/Application.cpp
+++ Ares/src/Ares/Application.cpp
@@ -1,20 +1,42 @@
 #include "AresPCH.h"
 
 #include "Application.h"
-#include "Ares/Events/ApplicationEvent.h"
+//#include "Ares/Events/ApplicationEvent.h"
 #include "Ares/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Ares {
+
+
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+
     Application::Application() 
     {
         m_Window = std::unique_ptr<Window>(Window::Create());
+        m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
     }
 
     Application::~Application() 
     {
 
+    }
+
+    void Application::OnEvent(Event& e)
+    {
+        EventDispatcher dispatcher(e);
+
+        // if dispatcher sees window close event, dispatch it to
+        // OnWindowClose
+        dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+
+        ARES_CORE_INFO("{0}", e);
+    }
+
+    bool Application::OnWindowClose(WindowCloseEvent& e)
+    {
+        m_Running = false;
+        return true;
     }
 
     void Application::Run() 
--- Ares/src/Ares/Application.h
+++ Ares/src/Ares/Application.h
@@ -1,6 +1,12 @@
 #pragma once
+
+#include "AresPCH.h"
+
 #include "Core.h"
-//#include "Events/Event.h"
+#include "Events/Event.h"
+
+#include "Ares/Events/ApplicationEvent.h"
+
 #include "Window.h "
 namespace Ares {
 
@@ -10,7 +16,11 @@ namespace Ares {
         Application();
         virtual ~Application();
         void Run();
+
+        void OnEvent(Event& e);
     private:
+        bool OnWindowClose(WindowCloseEvent& e);
+
         std::unique_ptr<Window> m_Window;
         bool m_Running = true;
     };
