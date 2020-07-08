c8dd59eb879a1ca8521078e0cf31b61793efc21a
Andres <andresgomezjr89@gmail.com>
2020-07-08 01:50:57 -0400

    incorporated ares events system to feed imgui events through imgui layer

--- Ares/src/Ares/ImGui/ImGuiLayer.h
+++ Ares/src/Ares/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Ares/Layer.h"
+#include "Ares/Events/ApplicationEvent.h"
+#include "Ares/Events/KeyEvent.h"
+#include "Ares/Events/MouseEvent.h"
 
 namespace Ares {
 	class ARES_API ImGuiLayer : public Layer
@@ -13,6 +16,18 @@ namespace Ares {
 		void OnUpdate();
 		void OnEvent(Event& event);
 	private:
+
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 		float m_Time = 0.0f;
 	};
 }
