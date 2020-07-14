261ccf10dabae3713c5e71d4ba612185a537a813
Andres <andresgomezjr89@gmail.com>
2020-07-14 14:19:25 -0400

    made input singleton

--- Ares/src/Ares/Core/Input.h
+++ Ares/src/Ares/Core/Input.h
@@ -6,6 +6,9 @@ namespace Ares {
 	class ARES_API Input
 	{
 	public:
+		Input(const Input&) = delete;
+		Input& operator=(const Input&) = delete;
+
 		inline static bool IsKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
 		inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }		
 		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
@@ -14,6 +17,9 @@ namespace Ares {
 		inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
 
 	protected:
+		Input() = default;
+
+
 		virtual bool IsKeyPressedImpl(int keycode) = 0;
 		virtual bool IsMouseButtonPressedImpl(int button) = 0;
 		virtual float GetMouseXImpl() = 0;
