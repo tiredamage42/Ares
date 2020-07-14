5376ff70a7c9e5f5d61c5e1f535f2ab46fb2bce9
Andres <andresgomezjr89@gmail.com>
2020-07-14 01:20:20 -0400

    now asserting opengl version >= 4.5

--- Ares/src/Platform/OpenGL/OpenGLContext.cpp
+++ Ares/src/Platform/OpenGL/OpenGLContext.cpp
@@ -21,6 +21,16 @@ namespace Ares {
 		ARES_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
 		ARES_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
 		ARES_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+
+
+#ifdef ARES_ENABLE_ASSERTS
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		ARES_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Ares requires at least OpenGL version 4.5!");
+#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
