eb6936490061f5796dc1f1d4d28d256018c5aedc
Andres <andresgomezjr89@gmail.com>
2020-07-15 13:03:34 -0400

    profiler fixes

--- Ares/src/Ares/Debug/Profiling.h
+++ Ares/src/Ares/Debug/Profiling.h
@@ -26,7 +26,7 @@ namespace Ares {
     {
         std::string Name;
         long long Start, End;
-        uint32_t ThreadID;
+        std::thread::id ThreadID;
     };
 
     //struct ProfilingSession
@@ -37,9 +37,11 @@ namespace Ares {
     class Profiling
     {
     private:
+        inline static std::mutex m_Mutex;
         //ProfilingSession* m_CurrentSession;
         inline static std::ofstream m_OutputStream;
-        inline static int m_ProfileCount;// = 0;
+        inline static bool m_CurrentProfile = false;
+        //inline static int m_ProfileCount = 0;
     public:
         //Profiling() : 
             //m_CurrentSession(nullptr), 
@@ -49,31 +51,65 @@ namespace Ares {
 
         static void BeginSession(const std::string& name, const std::string& filepath = "results.json")
         {
-            m_OutputStream.open(filepath);
-            WriteHeader();
 
-            m_ProfileCount = 0;
+            std::lock_guard lock(m_Mutex);
+            if (m_CurrentProfile) {
+                // If there is already a current session, then close it before beginning new one.
+                // Subsequent profiling output meant for the original session will end up in the
+                // newly opened session instead.  That's better than having badly formatted
+                // profiling output.
+                
+                // Edge case: BeginSession() might be before Log::Init()
+                if (Log::GetCoreLogger()) { 
+                    ARES_CORE_ERROR("Profiler::BeginSession('{0}') when another session is already open.", name);
+                }
+                
+                InternalEndSession();
+            }
+
+
+            m_OutputStream.open(filepath);
+
+            if (m_OutputStream.is_open()) {
+                m_CurrentProfile = true;
+                //m_CurrentSession = new InstrumentationSession({ name });
+                WriteHeader();
+            }
+            else {
+                // Edge case: BeginSession() might be before Log::Init()
+                if (Log::GetCoreLogger()) { 
+                    ARES_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+                }
+            }
+
+            /*WriteHeader();
+            m_ProfileCount = 0;*/
             //m_CurrentSession = new ProfilingSession{ name };
         }
 
         static void EndSession()
         {
-            WriteFooter();
-            m_OutputStream.close();
-            //delete m_CurrentSession;
-            //m_CurrentSession = nullptr;
-            m_ProfileCount = 0;
+            std::lock_guard lock(m_Mutex);
+            InternalEndSession();
+
+            //WriteFooter();
+            //m_OutputStream.close();
+            ////delete m_CurrentSession;
+            ////m_CurrentSession = nullptr;
+            //m_ProfileCount = 0;
         }
 
         static void WriteProfile(const ProfileResult& result)
         {
-            if (m_ProfileCount++ > 0)
-                m_OutputStream << ",";
+            /*if (m_ProfileCount++ > 0)
+                m_OutputStream << ",";*/
+
+            std::stringstream json;
 
             std::string name = result.Name;
             std::replace(name.begin(), name.end(), '"', '\'');
 
-            m_OutputStream << "{";
+            /*m_OutputStream << "{";
             m_OutputStream << "\"cat\":\"function\",";
             m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
             m_OutputStream << "\"name\":\"" << name << "\",";
@@ -83,12 +119,29 @@ namespace Ares {
             m_OutputStream << "\"ts\":" << result.Start;
             m_OutputStream << "}";
 
-            m_OutputStream.flush();
+            m_OutputStream.flush();*/
+
+            json << ",{";
+            json << "\"cat\":\"function\",";
+            json << "\"dur\":" << (result.End - result.Start) << ',';
+            json << "\"name\":\"" << name << "\",";
+            json << "\"ph\":\"X\",";
+            json << "\"pid\":0,";
+            json << "\"tid\":" << result.ThreadID << ",";
+            json << "\"ts\":" << result.Start;
+            json << "}";
+
+            std::lock_guard lock(m_Mutex);
+            if (m_CurrentProfile) {
+                m_OutputStream << json.str();
+                m_OutputStream.flush();
+            }
         }
 
         static void WriteHeader()
         {
-            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+            //m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
             m_OutputStream.flush();
         }
 
@@ -96,6 +149,19 @@ namespace Ares {
         {
             m_OutputStream << "]}";
             m_OutputStream.flush();
+        }
+
+        // Note: you must already own lock on m_Mutex before
+        // calling InternalEndSession()
+        void InternalEndSession() {
+            if (m_CurrentProfile) {
+                WriteFooter();
+                m_OutputStream.close();
+
+                m_CurrentProfile = false;
+                //delete m_CurrentSession;
+                //m_CurrentSession = nullptr;
+            }
         }
 
         /*static Profiling& Get()
@@ -127,8 +193,10 @@ namespace Ares {
             long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
             long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
 
-            uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-            Profiling::WriteProfile({ m_Name, start, end, threadID });
+            //uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
+            //Profiling::WriteProfile({ m_Name, start, end, threadID });
+
+            Profiling::WriteProfile({ m_Name, start, end, std::this_thread::get_id() });
 
             m_Stopped = true;
         }
