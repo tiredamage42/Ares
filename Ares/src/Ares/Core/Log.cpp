464565b18c2080c43ca1f9667b7beb192aa9004d
Andres <andresgomezjr89@gmail.com>
2020-07-15 13:35:06 -0400

    logger now logs to file as well

--- Ares/src/Ares/Core/Log.cpp
+++ Ares/src/Ares/Core/Log.cpp
@@ -4,6 +4,7 @@
 // suppress warnings
 #pragma warning(push, 0)
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 #pragma warning(pop)
 
 namespace Ares {
@@ -11,12 +12,29 @@ namespace Ares {
 	Ref<spdlog::logger> Log::s_ClientLogger;
 
 	void Log::Init() {
-		spdlog::set_pattern("%^[%l][%T] %n: %v %g%#%!%$");
-	
-		s_CoreLogger = spdlog::stdout_color_mt("ARES");
-		s_CoreLogger->set_level(spdlog::level::trace);
 
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Ares.log", true));
+
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+		//spdlog::set_pattern("%^[%l][%T] %n: %v %g%#%!%$");
+
+
+		s_CoreLogger = std::make_shared<spdlog::logger>("ARES", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
+		//s_CoreLogger = spdlog::stdout_color_mt("ARES");
+	
+		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
+
+
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
+		//s_ClientLogger = spdlog::stdout_color_mt("APP");
+
 		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 }
