61d46483825669b63cf69ae2daf4215d1429a533
Andres <andresgomezjr89@gmail.com>
2020-07-16 12:04:24 -0400

    renderer2D quadvertices now stored in mat4

--- Ares/src/Ares/Renderer/Renderer2D.cpp
+++ Ares/src/Ares/Renderer/Renderer2D.cpp
@@ -5,7 +5,6 @@
 
 #include "Ares/Renderer/RenderCommand.h"
 
-
 #include <GLFW/glfw3.h>
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -30,7 +29,6 @@ namespace Ares
 		
 		Ref<VertexArray> QuadVertexArray;
 		Ref<VertexBuffer> QuadVertexBuffer;
-
 		
 		Ref<Shader> TextureShader;
 		Ref<Texture2D> WhiteTexture;
@@ -44,22 +42,15 @@ namespace Ares
 		std::array<Ref<Texture2D>, MaxTextureSlots> TextureSlots;
 		uint32_t TextureSlotIndex = 1; // 0 = white texture
 
-		glm::vec4 QuadVertices[4];
+		glm::mat4 QuadVertices;
 
-		
 		Renderer2D::Statistics Stats;
-
 	};
-
 
 	static Renderer2DData s_Data;
 
 	void Renderer2D::ResetStats()
 	{
-
-		//s_Data.Stats.DrawCalls = 0;
-		//s_Data.Stats.QuadCount = 0;
-		//s_Data.Stats.TextureCount = 0;
 		memset(&s_Data.Stats, 0, sizeof(Renderer2D::Statistics));
 	}
 	Renderer2D::Statistics Renderer2D::GetStats()
@@ -71,17 +62,15 @@ namespace Ares
 	{
 		ARES_PROFILE_FUNCTION();
 
-		
 		s_Data.QuadVertexArray = VertexArray::Create();
 
-		/*float squareVertices[5 * 4] = {
+		/*float quadVertices[5 * 4] = {
 			-0.5f, -0.5f, 0.0f,     0.0f, 0.0f,
 			 0.5f, -0.5f, 0.0f,     1.0f, 0.0f,
 			 0.5f,  0.5f, 0.0f,     1.0f, 1.0f,
 			-0.5f,  0.5f, 0.0f,     0.0f, 1.0f
 		};*/
 
-		//Ref<VertexBuffer> squareVB = VertexBuffer::Create(squareVertices, sizeof(squareVertices));
 		s_Data.QuadVertexBuffer = VertexBuffer::Create(s_Data.MaxVertices * sizeof(QuadVertex));
 
 		s_Data.QuadVertexBuffer->SetLayout({
@@ -94,13 +83,12 @@ namespace Ares
 
 		s_Data.QuadVertexArray->AddVertexBuffer(s_Data.QuadVertexBuffer);
 
-
 		s_Data.QuadVertexBufferBase = new QuadVertex[s_Data.MaxVertices];
 		
 		uint32_t* quadIndicies = new uint32_t[s_Data.MaxIndicies];
 		
-
 		uint32_t offset = 0;
+		//uint32_t quadIndicies[6] = { 0, 1, 2, 2, 3, 0 };
 		for (uint32_t i = 0; i < s_Data.MaxIndicies; i+=6)
 		{
 			quadIndicies[i + 0] = offset + 0;
@@ -114,7 +102,6 @@ namespace Ares
 			offset += 4;
 		}
 		
-		//uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
 		Ref<IndexBuffer> quadIB = IndexBuffer::Create(quadIndicies, s_Data.MaxIndicies);
 		s_Data.QuadVertexArray->SetIndexBuffer(quadIB);
 		delete[] quadIndicies;
@@ -125,8 +112,6 @@ namespace Ares
 		uint32_t whiteTextureData = 0xffffffff;
 		s_Data.WhiteTexture->SetData(&whiteTextureData, sizeof(uint32_t));
 
-
-
 		int32_t samplers[s_Data.MaxTextureSlots];
 		for (uint32_t i = 0; i < s_Data.MaxTextureSlots; i++)
 			samplers[i] = i;
@@ -136,19 +121,16 @@ namespace Ares
 		s_Data.TextureShader->Bind();
 
 		s_Data.TextureShader->SetIntArray("u_Textures", samplers, s_Data.MaxTextureSlots);
-		//s_Data.TextureShader->SetInt("u_Texture", 0);
-
-		// set all texture slots to 0
-		/*for (uint32_t i = 0; i < s_Data.TextureSlots.size(); i++)
-			s_Data.TextureSlots[i] = 0;*/
-
-
+		
 		s_Data.TextureSlots[0] = s_Data.WhiteTexture;
 
-		s_Data.QuadVertices[0] = { -0.5f, -0.5f, 0.0f, 1.0f };
-		s_Data.QuadVertices[1] = {  0.5f, -0.5f, 0.0f, 1.0f };
-		s_Data.QuadVertices[2] = {  0.5f,  0.5f, 0.0f, 1.0f };
-		s_Data.QuadVertices[3] = { -0.5f,  0.5f, 0.0f, 1.0f };
+		s_Data.QuadVertices = { 
+			{ -0.5f, -0.5f, 0.0f, 1.0f },
+			{  0.5f, -0.5f, 0.0f, 1.0f },
+			{  0.5f,  0.5f, 0.0f, 1.0f },
+			{ -0.5f,  0.5f, 0.0f, 1.0f } 
+		};
+
 
 
 	}
@@ -271,9 +253,12 @@ namespace Ares
 
 		transform = glm::scale(transform, { size.x, size.y, 1.0f });
 
+		transform = transform * s_Data.QuadVertices;
+
 		for (uint32_t i = 0; i < 4; i++)
 		{
-			s_Data.QuadVertexBufferPtr->Position = transform * s_Data.QuadVertices[i];
+			s_Data.QuadVertexBufferPtr->Position = transform[i];
+
 			s_Data.QuadVertexBufferPtr->Color = color;
 			s_Data.QuadVertexBufferPtr->TexCoord = { (float)(i == 1 || i == 2), (float)(i > 1) };
 			s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
