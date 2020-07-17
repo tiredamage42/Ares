f5f08d0cc7c90fa8651d6b9dbe5f45ac20473eda
Andres <andresgomezjr89@gmail.com>
2020-07-17 15:29:04 -0400

    added to scene renderer

--- Ares/src/Ares/Renderer/SceneRenderer.cpp
+++ Ares/src/Ares/Renderer/SceneRenderer.cpp
@@ -1,12 +1,145 @@
 #include "SceneRenderer.h"
 
+#include "Renderer.h"
+
+#include <glad/glad.h>
+#include <glm/gtc/matrix_transform.hpp>
+
 namespace Ares
 {
+	struct SceneRendererData
+	{
+		const Scene* ActiveScene = nullptr;
+		struct SceneInfo
+		{
+			Camera SceneCamera;
+
+			// Resources
+			Ref<MaterialInstance> SkyboxMaterial;
+			Ref<TextureCube> EnvironmentRadianceMap;
+			Ref<TextureCube> EnvironmentIrradianceMap;
+
+
+		} SceneData;
+
+		Ref<Texture2D> BRDFLUT;
+		Ref<Shader> CompositeShader;
+
+		Ref<RenderPass> GeoPass;
+		Ref<RenderPass> CompositePass;
+
+		struct DrawCommand
+		{
+			Ref<Mesh> Mesh;
+			Ref<MaterialInstance> Material;
+			glm::mat4 Transform;
+		};
+
+		std::vector<DrawCommand> DrawList;
+
+		//grid
+		Ref<MaterialInstance> GridMaterial;
+	};
+
+	static SceneRendererData s_Data;
+
+	void SceneRenderer::Init()
+	{
+		FrameBufferSpecs geoFrameBufferSpecs;
+		geoFrameBufferSpecs.Width = 1280;
+		geoFrameBufferSpecs.Height = 720;
+		geoFrameBufferSpecs.Format = FramebufferFormat::RGBA16F;
+		geoFrameBufferSpecs.ClearColor = { .1f, .1f, .1f, 1.0f };
+
+		RenderPassSpecs geoRenderPassSpecs;
+		geoRenderPassSpecs.TargetFrameBuffer = FrameBuffer::Create(geoFrameBufferSpecs);
+		s_Data.GeoPass = RenderPass::Create(geoRenderPassSpecs);
+
+
+
+		FrameBufferSpecs compFrameBufferSpecs;
+		compFrameBufferSpecs.Width = 1280;
+		compFrameBufferSpecs.Height = 720;
+		compFrameBufferSpecs.Format = FramebufferFormat::RGBA8;
+		compFrameBufferSpecs.ClearColor = { .5f, .1f, .1f, 1.0f };
+
+		RenderPassSpecs compRenderPassSpecs;
+		compRenderPassSpecs.TargetFrameBuffer = FrameBuffer::Create(compFrameBufferSpecs);
+		s_Data.CompositePass = RenderPass::Create(compRenderPassSpecs);
+
+
+		s_Data.CompositeShader = Shader::Create("Assets/Shaders/hdr.glsl");
+		s_Data.BRDFLUT = Texture2D::Create("Assets/Textures/BRDF_LUT.tga");
+
+		// grid
+		auto gridShader = Shader::Create("Assets/Shaders/Grid.glsl");
+		s_Data.GridMaterial = MaterialInstance::Create(Material::Create(gridShader));
+		float gridScale = 16.025f, gridSize = .025f;
+		s_Data.GridMaterial->Set("u_Scale", gridScale);
+		s_Data.GridMaterial->Set("u_Res", gridSize);
+	}
+
+	void SceneRenderer::SetViewportSize(uint32_t width, uint32_t height)
+	{
+		s_Data.GeoPass->GetSpecs().TargetFrameBuffer->Resize(width, height);
+		s_Data.CompositePass->GetSpecs().TargetFrameBuffer->Resize(width, height);
+	}
+
+	void SceneRenderer::BeginScene(const Scene* scene)
+	{
+		ARES_CORE_ASSERT(!s_Data.ActiveScene, "");
+		s_Data.ActiveScene = scene;
+
+		s_Data.SceneData.SceneCamera = scene->m_Camera;
+		s_Data.SceneData.SkyboxMaterial = scene->m_SkyboxMaterial;
+		s_Data.SceneData.EnvironmentRadianceMap = scene->m_EnvironmentRadianceMap;
+		s_Data.SceneData.EnvironmentIrradianceMap = scene->m_EnvironmentIrradianceMap;
+	}
+
+
 	void SceneRenderer::EndScene()
 	{
 		ARES_CORE_ASSERT(s_Data.ActiveScene, "");
 		s_Data.ActiveScene = nullptr;
 		FlushDrawList();
+	}
+
+	void SceneRenderer::GeometryPass()
+	{
+		Renderer::BeginRenderPass(s_Data.GeoPass);
+		auto viewProjection = s_Data.SceneData.SceneCamera.GetProjectionMatrix() * s_Data.SceneData.SceneCamera.GetViewMatrix();
+
+		// skybox
+		auto skyboxShader = s_Data.SceneData.SkyboxMaterial->GetShader();
+		s_Data.SceneData.SkyboxMaterial->Set("u_InverseVP", glm::inverse(viewProjection));
+		//s_Data.SceneInfo.EnvironmentIrradianceMap->Bind(0);
+		Renderer::SubmitFullScreenQuad(s_Data.SceneData.SkyboxMaterial);
+
+		// render entities
+		for (auto& dc : s_Data.DrawList)
+		{
+			dc.Material->Set("u_ViewProjectionMatrix", viewProjection);
+			dc.Material->Set("u_CameraPosition", s_Data.SceneData.SceneCamera.GetPosition());
+
+			// environment (todo: dont do this per mesh)
+			dc.Material->Set("u_EnvRadianceTex", s_Data.SceneData.EnvironmentRadianceMap);
+			dc.Material->Set("u_EnvIrradianceTex", s_Data.SceneData.EnvironmentIrradianceMap);
+			dc.Material->Set("u_BRDFLUTTexture", s_Data.BRDFLUT);
+
+			Renderer::SubmitMesh(dc.Mesh, dc.Transform, dc.Material);
+		}
+
+		// grid
+		s_Data.GridMaterial->Set("u_ViewProjectionMatrix", viewProjection);
+
+		Renderer::SubmitQuad(s_Data.GridMaterial, glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(1.0f,0.0f,0.0f)) * glm::scale(glm::mat4(1.0f), ))
+
+		Renderer::EndRenderPass();
+
+	}
+
+	void SceneRenderer::CompositePass()
+	{
 	}
 
 	void SceneRenderer::SubmitEntity(Entity* entity)
--- Ares/src/Ares/Renderer/SceneRenderer.h
+++ Ares/src/Ares/Renderer/SceneRenderer.h
@@ -1,17 +1,23 @@
 #pragma once
 
 #include "Ares/Scene/Entity.h"
+#include "Ares\Scene\Scene.h"
 
 namespace Ares
 {
 	class SceneRenderer
 	{
 	public:
+		static void Init();
+		static void CreateEnvironmentMap(const std::string& filepath);
+
+		static void SetViewportSize(uint32_t width, uint32_t height);
+		static void BeginScene(const Scene* scene);
 		static void SubmitEntity(Entity* entity);
 		static void EndScene();
-		static void CreateEnvironmentMap(const std::string& filepath);
 		
-
+		static void GeometryPass();
+		static void CompositePass();
 	private:
 
 	};
