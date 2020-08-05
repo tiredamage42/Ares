23bbc065f0a2ad3149535d26d5d0909d8ada04d3
Andres <andresgomezjr89@gmail.com>
2020-08-05 16:25:28 -0400

    removed scene camera logs

--- Ares/src/Ares/Renderer/SceneCamera.cpp
+++ Ares/src/Ares/Renderer/SceneCamera.cpp
@@ -18,7 +18,6 @@ namespace Ares
 	void SceneCamera::SetViewportSize(uint32_t width, uint32_t height)
 	{
 
-		ARES_CORE_LOG("Setting viewport size camera {0} / {1}", width, height);
 		m_AspectRatio = (float)width / (float)height;
 		RecalculateProjection();
 	}
@@ -29,8 +28,6 @@ namespace Ares
 		float r =  m_OrthoSize * m_AspectRatio * .5f;
 		float b = -m_OrthoSize * .5f;
 		float t =  m_OrthoSize * .5f;
-
-		ARES_CORE_LOG("l {0}, r {1}, b {2}, t {3}, n {4}, f {5}", l, r, b, t, m_OrthoNear, m_OrthoFar);
 
 		m_ProjectionMatrix = glm::ortho(l, r, b, t, m_OrthoNear, m_OrthoFar);
 	
