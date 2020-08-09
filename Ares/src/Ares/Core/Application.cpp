f8336ad12a13e51891042703fc20d00dc1593e4e
Andres <andresgomezjr89@gmail.com>
2020-08-09 18:20:14 -0400

    added save file and edited open file in application

--- Ares/src/Ares/Core/Application.cpp
+++ Ares/src/Ares/Core/Application.cpp
@@ -135,7 +135,7 @@ namespace Ares {
         }
     }
 
-    std::string Application::OpenFile(const std::string& filter) const
+    std::string Application::OpenFile(const char* filter) const
     {
         OPENFILENAMEA ofn;       // common dialog box structure
         CHAR szFile[260] = { 0 };       // if using TCHAR macros
@@ -146,14 +146,37 @@ namespace Ares {
         ofn.hwndOwner = glfwGetWin32Window((GLFWwindow*)m_Window->GetNativeWindow());
         ofn.lpstrFile = szFile;
         ofn.nMaxFile = sizeof(szFile);
-        ofn.lpstrFilter = "All\0*.*\0";
+        ofn.lpstrFilter = filter;
+
+        //ofn.lpstrFilter = "All\0*.*\0";
         ofn.nFilterIndex = 1;
-        ofn.lpstrFileTitle = NULL;
-        ofn.nMaxFileTitle = 0;
-        ofn.lpstrInitialDir = NULL;
-        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+        //ofn.lpstrFileTitle = NULL;
+        //ofn.nMaxFileTitle = 0;
+        //ofn.lpstrInitialDir = NULL;
+        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
 
         if (GetOpenFileNameA(&ofn) == TRUE)
+        {
+            return ofn.lpstrFile;
+        }
+        return std::string();
+    }
+    std::string Application::SaveFile(const char* filter) const
+    {
+        OPENFILENAMEA ofn;       // common dialog box structure
+        CHAR szFile[260] = { 0 };       // if using TCHAR macros
+
+        // Initialize OPENFILENAME
+        ZeroMemory(&ofn, sizeof(OPENFILENAME));
+        ofn.lStructSize = sizeof(OPENFILENAME);
+        ofn.hwndOwner = glfwGetWin32Window((GLFWwindow*)m_Window->GetNativeWindow());
+        ofn.lpstrFile = szFile;
+        ofn.nMaxFile = sizeof(szFile);
+        ofn.lpstrFilter = filter;
+        ofn.nFilterIndex = 1;
+        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+
+        if (GetSaveFileNameA(&ofn) == TRUE)
         {
             return ofn.lpstrFile;
         }
