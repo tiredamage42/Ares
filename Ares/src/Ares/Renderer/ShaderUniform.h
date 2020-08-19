2bab372de83134815f1be24ac5d96ded006aec04
Andres <andresgomezjr89@gmail.com>
2020-08-19 18:36:39 -0400

    cleaned up shader uniform classes

--- Ares/src/Ares/Renderer/ShaderUniform.h
+++ Ares/src/Ares/Renderer/ShaderUniform.h
@@ -3,7 +3,6 @@
 #include "Ares/Core/Log.h"
 #include <vector>
 #include <string>
-
 #include <glm/glm.hpp>
 
 namespace Ares
@@ -32,100 +31,34 @@ namespace Ares
 		}
 	};
 
-
-
-	/*enum class ShaderDomain
-	{
-		None = 0, Vertex = 0, Pixel = 1
-	};*/
-
 	class ShaderUniformDeclaration
 	{
-	private:
-		//friend class Shader;
-		//friend class OpenGLShader;
-		//friend class ShaderStruct;
 	public:
 		enum class Type
 		{
-			NONE, FLOAT32, VEC2, VEC3, VEC4, MAT3, MAT4, INT32//, STRUCT
+			NONE, FLOAT32, VEC2, VEC3, VEC4, MAT3, MAT4, INT32
 		};
 
 		UniformAttributes m_Attributes;
-
 		virtual const std::string& GetName() const = 0;
-		//virtual const size_t GetHashName() const = 0;
-
-
 		virtual Type GetType() const = 0;
-
-		//virtual bool HasDefaultValue() const = 0;
-		//virtual byte* GetDefaultValue() const = 0;
-
 		virtual uint32_t GetSize() const = 0;
 		virtual uint32_t GetCount() const = 0;
 		virtual uint32_t GetOffset() const = 0;
-		//virtual ShaderDomain GetDomain() const = 0;
 	protected:
 		virtual void SetOffset(uint32_t offset) = 0;
 	};
-
 	typedef std::vector<ShaderUniformDeclaration*> ShaderUniformList;
 
-	/*
-	*/
 	class ShaderUniformBufferDeclaration
 	{
 	public:
 		virtual const std::string& GetName() const = 0;
-		//virtual uint32_t GetRegister() const = 0;
 		virtual uint32_t GetSize() const = 0;
 		virtual const ShaderUniformList& GetUniformDeclarations() const = 0;
-
 		virtual ShaderUniformDeclaration* FindUniform(const std::string& name) = 0;
 	};
 
-	//typedef std::vector<ShaderUniformBufferDeclaration*> ShaderUniformBufferList;
-
-	/*
-	class ShaderStruct
-	{
-	private:
-		//friend class Shader;
-	private:
-		std::string m_Name;
-		std::vector<ShaderUniformDeclaration*> m_Fields;
-		uint32_t m_Size;
-		uint32_t m_Offset;
-	public:
-		ShaderStruct(const std::string& name)
-			: m_Name(name), m_Size(0), m_Offset(0)
-		{
-		}
-
-		void AddField(ShaderUniformDeclaration* field)
-		{
-			m_Size += field->GetSize();
-			uint32_t offset = 0;
-			if (m_Fields.size())
-			{
-				ShaderUniformDeclaration* previous = m_Fields.back();
-				offset = previous->GetOffset() + previous->GetSize();
-			}
-			field->SetOffset(offset);
-			m_Fields.push_back(field);
-		}
-
-		inline void SetOffset(uint32_t offset) { m_Offset = offset; }
-
-		inline const std::string& GetName() const { return m_Name; }
-		inline uint32_t GetSize() const { return m_Size; }
-		inline uint32_t GetOffset() const { return m_Offset; }
-		inline const std::vector<ShaderUniformDeclaration*>& GetFields() const { return m_Fields; }
-	};
-
-	typedef std::vector<ShaderStruct*> ShaderStructList;
-	*/
 
 	class ShaderResourceDeclaration
 	{
@@ -137,32 +70,18 @@ namespace Ares
 		};
 		UniformAttributes m_Attributes;
 		virtual Type GetType() const = 0;
-
 		virtual const std::string& GetName() const = 0;
-		//virtual const size_t GetHashName() const = 0;
-
 		virtual uint32_t GetRegister() const = 0;
-		//virtual uint32_t GetCount() const = 0;
 	};
 
 	class ShaderResourceArrayDeclaration
 	{
-
 	public:
-		/*enum class Type
-		{
-			NONE, TEXTURE2D, TEXTURECUBE
-		};*/
 		UniformAttributes m_Attributes;
 		virtual ShaderResourceDeclaration::Type GetType() const = 0;
-
 		virtual const std::string& GetName() const = 0;
-		//virtual const size_t GetHashName() const = 0;
-
-		//virtual uint32_t GetRegister() const = 0;
 		virtual uint32_t GetCount() const = 0;
 	};
-
 
 	typedef std::vector<ShaderResourceDeclaration*> ShaderResourceList;
 	typedef std::vector<ShaderResourceArrayDeclaration*> ShaderResourceArrayList;
